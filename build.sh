e8aca51707ba0f5767daab495ad5ebd9812eb8e9
Jason <jason@eigpay.com>
2026-03-15 17:59:28 +0800

    v4.1.0.1

--- build.sh
+++ build.sh
@@ -13,5 +13,7 @@ if test $datetime != ""; then
   docker tag $tag:latest $tag:$datetime
 
   echo "在宿主机上使用如下指令推送镜像到仓库"
-  echo "skopeo copy --dest-creds cn-east-2@\${SWR_AK}:\${SWR_PW} docker-daemon:$tag:$datetime docker://$tag:$datetime"
+  echo "skopeo copy --dest-creds cn-east-2@\$SWR_AK:\$SWR_PW docker-daemon:$tag:$datetime docker://$tag:$datetime"
+  echo "或"
+  echo "docker run -v /var/run/docker.sock:/var/run/docker.sock quay.io/skopeo/stable:v1.22 copy --dest-creds cn-east-2@$SWR_AK:$SWR_PW docker-daemon:$tag:$datetime docker://$tag:$datetime"
 fi
